720a3129fb79be2acc49042ec51d156b488f5d1c
余文彪 <2383299053@qq.com>
2017-07-17 17:28:00 +0800

    注释

--- com/dao/UserDAO.java
+++ com/dao/UserDAO.java
@@ -2,9 +2,6 @@ package com.dao;
 
 import com.entity.User;
 
-/**
- * Created by 余文彪 on 2017/7/13.
- */
 public interface UserDAO {
-    public User findUserByUseridAndUser_passwod(String user_id,String user_password);
+    public User findUserByUseridAndUser_passwod(String user_id, String user_password);
 }
